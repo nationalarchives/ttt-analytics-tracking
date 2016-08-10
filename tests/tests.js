2da163da572c522271e920604c04250fdbea9c47
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-08-10 13:39:01 +0100

    Add functionality for event on load
    Introduces the load event and time tracking.

--- tests/tests.js
+++ tests/tests.js
@@ -1,15 +1,33 @@
-QUnit.test( 'Bootstrap', function( assert ) {
+QUnit.test('Bootstrap', function (assert) {
     assert.equal(typeof $, 'function', 'jQuery exists');
     assert.ok($('#qunit-fixture').length == 1, 'The qUnit fixture is available');
     assert.equal(typeof ttt_analytics, 'object', 'the ttt_analytics object is available');
     assert.equal(typeof ttt_analytics.webTrendsProxy, 'function', 'webTrendsProxyForTTT is defined as a function');
 });
 
+
+QUnit.test('Event is fired when page has loaded', function (assert) {
+    var done = assert.async();
+    setTimeout(function () {
+        assert.equal(ttt_analytics.load_event_called, true);
+        done();
+    }, 500);
+});
+
+QUnit.test('Load time is recorded in object', function (assert) {
+    var done = assert.async();
+    setTimeout(function () {
+        assert.notEqual(ttt_analytics.load_time, false, "The load time is not false");
+        assert.ok(/^\d*$/.test(ttt_analytics.load_time), true, "The load time is digits only");
+        done();
+    }, 500);
+});
+
 // Specific dcsMultiTrack events to be fired at specific points:
 
 // DONE - stub configurable proxy for WebTrends calling function
-// TODO - fire dcsMultiTrack event when the page is loaded
-// TODO     - record time when page was loaded
+// DONE - fire dcsMultiTrack event when the page is loaded
+// DONE     - record time when page was loaded
 // TODO - fire dcsMultiTrack event when a user starts scrolling
 // TODO     - event should send the time between load and begin of scroll
 // TODO - fire event when user scrolling reaches the TTT widget
--- ttt-analytics-events.js
+++ ttt-analytics-events.js
@@ -2,11 +2,22 @@
 
 var ttt_analytics = (function() {
 
-    var webTrendsProxy = function() {
-
-    };
-
     return {
-        webTrendsProxy : webTrendsProxy
+        load_event_called : false,
+        load_time : false,
+        webTrendsProxy : function(arg) {
+            switch (arg) {
+                case 'load-event':
+                    // webTrendsCall for load event
+                    this.load_event_called = true;
+                    this.load_time = Date.now();
+                    break;
+
+            }
+        }
     }
 })();
+
+$(document).ready(function() {
+   ttt_analytics.webTrendsProxy('load-event')
+});
